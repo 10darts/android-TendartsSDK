4343bdb5db8e26a718db3ca7daaf4b5163e4a1ea
Luis Manuel Cotrino Benavides <>
2018-03-19 10:05:46 +0100

    Fixed constants file for debug.

--- src/sdkTesting/java/com/darts/sdk/common/ConstantsBase.java
+++ src/sdkTesting/java/com/darts/sdk/common/ConstantsBase.java
@@ -6,6 +6,6 @@ package com.tendarts.sdk.common;
 
 
 public class ConstantsBase {
-	public static final String baseUrl = "http://staging.10darts.com";
-	public static final boolean inTest = true;
+	public static final String BASE_URL = "http://staging.10darts.com";
+	public static final boolean IN_TEST = true;
 }
